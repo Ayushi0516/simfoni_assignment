12f31b4ee6e3ad96afc8ddf9f2f86fb9d72de145
Ayushi0516 <ayushigupta0516@gmail.com>
2023-12-04 15:52:12 +0530

    initial commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Flex } from '@chakra-ui/react'
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+  <Flex >
+   <h3>Logo</h3>
+   
+  </Flex>
+  )
+}
+
+export default Navbar
